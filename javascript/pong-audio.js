2b6c0905b095773262ccc4ff1a9f4fae79f96adc
Bribeee <133678440+Bribeee@users.noreply.github.com>
2023-05-25 15:02:24 -0500

    Update pong-audio.js
    background noise

--- javascript/pong-audio.js
+++ javascript/pong-audio.js
@@ -52,7 +52,7 @@ soundArray.push(paddleSound);
 export var scoreSound = new soundFile("silence.mp3");
 soundArray.push(scoreSound);
 
-export var ambientSound = new soundFile("onlymp3.to - Advent One-Winged Angel  Super Smash Bros. Ultimate (REUPLOAD)-2wIqK_zw5Vs-256k-1655706940250.mp3");
+export var ambientSound = new soundFile("OWA.mp3");
 soundArray.push(ambientSound);
 ambientSound.player.loop = true; //turn on looping
 ambientSound.player.volume.value = -20; //turn down volume
